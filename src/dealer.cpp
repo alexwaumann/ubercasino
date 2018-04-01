65bef98fde1fb614fdf1e33f0ad3b9f6e8b67871
Alejandro Waumann <awaumann@gmail.com>
2018-04-01 08:38:20 -0500

    added Dealer class

---
+++ src/dealer.cpp
@@ -0,0 +1,7 @@
+/*
+ * File: dealer.cpp
+ * Created: 04/01/2018
+ * Modified: 04/01/2018
+ */
+
+
---
+++ src/dealer.h
@@ -0,0 +1,11 @@
+/*
+ * file: dealer.h
+ * Created: 04/01/2018
+ * Modified: 04/01/2018
+ */
+
+class Dealer
+{
+    public:
+    private:
+}

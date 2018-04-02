fb8f67f3e75ff1a66f442c0ebc806bb03c2fd341
Alejandro Waumann <awaumann@gmail.com>
2018-04-01 22:57:20 -0500

    added Player class

---
+++ src/player.cpp
@@ -0,0 +1,63 @@
+/*
+ * File: player.cpp
+ * Created: 04/01/2018
+ * Modified: 04/01/2018
+ */
+
+#include "player.h"
+
+Player::Player( std::string uuid, std::string name )
+{
+    this->uuid = uuid;
+    this->name = name;
+    this->action = player_action_t::idle;
+    this->num_hands = 0;
+}
+
+std::string Player::get_uuid() { return this->uuid; }
+
+std::string Player::get_name() { return this->name; }
+
+player_action_t Player::get_action() { return this->action; }
+
+/*
+ * Function: get_hand
+ *
+ * Gets one of the player's hands.
+ *
+ * @param index: hand to get from hand array
+ *
+ * returns: hand
+ */
+hand_t Player::get_hand( int index )
+{
+    // TODO: raise exception here
+    if( index >= this->num_hands )
+        ;
+    return hands[index];
+}
+
+int Player::get_num_hands() { return this->num_hands; }
+
+void Player::set_action( player_action_t action ) { this->action = action; }
+
+/*
+ * Function: add_hand
+ *
+ * Adds a hand to the player's hand array if there is space
+ * for one.
+ *
+ * @param hand: hand to be added
+ *
+ * returns: true if successfully adds hand
+ *          false if unsuccessful
+ */
+bool Player::add_hand( hand_t hand )
+{
+    if( this->num_hands == MAX_HANDS_PER_PLAYER )
+        return false;
+    
+    hands[num_hands++] = hand;
+    return true;
+}
+
---
+++ src/player.h
@@ -0,0 +1,40 @@
+/*
+ * File: player.h
+ * Created: 04/01/2018
+ * Modified: 04/01/2018
+ */
+
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include <string>
+#include "hand_t.h"
+
+#define MAX_HANDS_PER_PLAYER    3
+
+enum class player_action_t { idle, hit, stand, split, double_down };
+
+class Player
+{
+    public:
+        Player( std::string uuid, std::string name );
+        
+        std::string get_uuid();
+        std::string get_name();
+        player_action_t get_action();
+        hand_t get_hand( int index );
+        int get_num_hands();
+
+        void set_action( player_action_t action );
+        bool add_hand( hand_t hand );
+
+    private:
+        std::string uuid;
+        std::string name;
+        player_action_t action;
+        hand_t hands[MAX_HANDS_PER_PLAYER];
+        int num_hands;
+};
+
+#endif
+

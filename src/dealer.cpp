5676f5a871c34f1a76f46ed3d0747cde39286d7d
Alejandro Waumann <awaumann@gmail.com>
2018-04-26 13:48:08 -0500

    starter dealer class

--- src/dealer.cpp
+++ src/dealer.cpp
@@ -4,4 +4,16 @@
  * Modified: 04/01/2018
  */
 
+#include<boost/thread.hpp>
+#include "dealer.h"
 
+dealer::dealer() 
+{
+}
+
+dealer::~dealer() { }
+
+void dealer::callback( ubercasino::Player data )
+{
+    std::cout << "Player payload received" << std::endl;
+}
--- src/dealer.h
+++ src/dealer.h
@@ -1,11 +1,28 @@
 /*
  * file: dealer.h
  * Created: 04/01/2018
- * Modified: 04/01/2018
+ * Modified: 04/02/2018
  */
 
-class Dealer
+#ifndef DEALER_H
+#define DEALER_H
+
+#include <string>
+#include <iostream>
+#include <boost/uuid/uuid.hpp>
+#include "ccpp_ubercasino.h"
+
+class dealer
 {
     public:
+        dealer();
+        ~dealer();
+        ubercasino::Dealer dealer_struct;
+        ubercasino::Game game_struct;
+        void callback( ubercasino::Player data );
+
+        void timer_expired();
     private:
-}
+};
+
+#endif

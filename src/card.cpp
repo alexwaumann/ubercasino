90fdce95d1757fcf7281f990958e59c48e0f4e5e
Alejandro Waumann <awaumann@gmail.com>
2018-05-03 20:29:30 -0500

    card class now uses IDL generated structs

--- src/card.cpp
+++ src/card.cpp
@@ -1,80 +1,25 @@
 /*
  * File: card.cpp
- * Created: 04/01/2018
- * Modified: 04/05/2018
  */
 
 #include "card.h"
 
-Card::Card()
+card::card( suite_t suit, card_kind rank )
 {
-    this->rank = rank_t::ace;
-    this->suit = suit_t::hearts;
-    this->face_up = false;
+    this->m_card.card = rank;
+    this->m_card.suite = suit;
+    this->m_card.valid = false;
 }
 
-Card::Card( const Card& card )
+card::card( suite_t suit, card_kind rank, bool valid )
+    : card( suit, rank )
 {
-    this->rank = card.rank;
-    this->suit = card.suit;
-    this->face_up = card.face_up;
+    this->m_card.valid = valid;
 }
 
-Card::Card( rank_t rank, suit_t suit )
-{
-    this->rank = rank;
-    this->suit = suit;
-    this->face_up = false;
-}
+suite_t card::suit() { return m_card.suite; }
 
-Card::Card( rank_t rank, suit_t suit, bool face_up )
-{
-    this->rank = rank;
-    this->suit = suit;
-    this->face_up = face_up;
-}
+card_kind card::rank() { return m_card.card; }
 
-/*
- * Function: get_rank
- *
- * returns: rank of the card
- */
-rank_t Card::get_rank() const { return this->rank; }
-
-/*
- * Function: get_suit
- *
- * returns: suit of the card
- */
-suit_t Card::get_suit() const { return this->suit; }
-
-/*
- * Function: is_face_up
- *
- * returns: true if card is facing up
- *          false otherwise
- */
-bool Card::is_face_up() const { return this->face_up; }
-
-/*
- * Function: is_face_up
- *
- * returns: true if card is facing up
- *          false otherwise
- */
-bool Card::is_ace() const { return this->rank == rank_t::ace; }
-
-/*
- * Function: set_face_up
- *
- * Sets the card to be facing up
- */
-void Card::set_face_up() { this->face_up = true; }
-
-/*
- * Function: set_face_down
- *
- * Sets the card to be facing down
- */
-void Card::set_face_down() { this->face_up = false; }
+bool card::valid() { return m_card.valid; }
 
--- src/card.h
+++ src/card.h
@@ -1,35 +1,26 @@
 /*
  * File: card.h
- * Created: 04/01/2018
- * Modified: 04/05/2018
  */
 
-#ifndef __CARD_H__
-#define __CARD_H__
+#ifndef CARD_H
+#define CARD_H
 
-#include "rank_t.h"
-#include "suit_t.h"
+#include "ccpp_ubercasino.h"
 
-class Card
+using namespace UberCasino;
+
+class card
 {
     public:
-        Card();
-        Card( const Card& card );
-        Card( rank_t rank, suit_t suit );
-        Card( rank_t rank, suit_t suit, bool face_up );
+        card( suite_t suit, card_kind rank );
+        card( suite_t suit, card_kind rank, bool valid );
 
-        rank_t get_rank() const;
-        suit_t get_suit() const;
-        bool is_face_up() const;
-        bool is_ace() const;
-
-        void set_face_up();
-        void set_face_down();
+        suite_t suit();
+        card_kind rank();
+        bool valid();
 
     private:
-        rank_t rank;
-        suit_t suit;
-        bool face_up;
+        card_t m_card; // uses card type from UberCasino namespace
 };
 
 #endif

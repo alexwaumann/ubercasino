cd38fb96d6f09ba68856c0ecdb9f168d35903e53
Alejandro Waumann <awaumann@gmail.com>
2018-05-03 21:42:14 -0500

    removed use of 'this'

--- src/card.cpp
+++ src/card.cpp
@@ -6,15 +6,15 @@
 
 Card::Card( suite_t suit, card_kind rank )
 {
-    this->m_card.card = rank;
-    this->m_card.suite = suit;
-    this->m_card.valid = false;
+    m_card.card = rank;
+    m_card.suite = suit;
+    m_card.valid = false;
 }
 
 Card::Card( suite_t suit, card_kind rank, bool valid )
     : Card( suit, rank )
 {
-    this->m_card.valid = valid;
+    m_card.valid = valid;
 }
 
 suite_t Card::suit() { return m_card.suite; }

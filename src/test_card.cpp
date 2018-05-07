b04fac4e41716d7988370231b66be8c987dfa12d
Alejandro Waumann <awaumann@gmail.com>
2018-05-06 23:28:28 -0500

    updated tests

--- src/test_card.cpp
+++ src/test_card.cpp
@@ -9,56 +9,39 @@
 
 using namespace std;
 
-void test_constructor( Card card, rank_t rank, suit_t suit, bool face_up );
+void test_constructor( Card card, rank_t rank, suit_t suit );
 
 int main( int argc, char ** argv )
 {
     Card ace_of_hearts;
-    Card six_of_spades{ rank_t::six, suit_t::spades };
-    Card queen_of_diamonds{ rank_t::queen, suit_t::diamonds, true };
+    Card six_of_spades{ suit_t::spades, rank_t::six };
+    Card queen_of_diamonds{ suit_t::diamonds, rank_t::queen };
     Card card_copy{ queen_of_diamonds };
 
     // test constructors
-    test_constructor( ace_of_hearts, rank_t::ace, suit_t::hearts, false );
-    test_constructor( six_of_spades, rank_t::six, suit_t::spades, false );
-    test_constructor( queen_of_diamonds, rank_t::queen, suit_t::diamonds, true );
-    test_constructor( card_copy, rank_t::queen, suit_t::diamonds, true );
+    test_constructor( ace_of_hearts, rank_t::ace, suit_t::hearts );
+    test_constructor( six_of_spades, rank_t::six, suit_t::spades );
+    test_constructor( queen_of_diamonds, rank_t::queen, suit_t::diamonds );
+    test_constructor( card_copy, rank_t::queen, suit_t::diamonds );
 
     // test is_ace()
-    if( !ace_of_hearts.is_ace() )
+    if( !ace_of_hearts.ace() )
         cout << "Error: is_ace()" << endl
              << "\tExpected: 1" << endl
-             << "\tActual  : " << ace_of_hearts.is_ace() << endl;
-
-    // test setters
-    ace_of_hearts.set_face_up();
-    if( !ace_of_hearts.is_face_up() )
-        cout << "Error: set_face_up()" << endl
-             << "\tExpected: 1" << endl
-             << "\tActual  : " << ace_of_hearts.is_face_up() << endl;
-
-    queen_of_diamonds.set_face_down();
-    if( queen_of_diamonds.is_face_up() )
-        cout << "Error: set_face_down()" << endl
-             << "\tExpected: 0" << endl
-             << "\tActual  : " << queen_of_diamonds.is_face_up() << endl;
+             << "\tActual  : " << ace_of_hearts.ace() << endl;
 
     return 0;
 }
 
-void test_constructor( Card card, rank_t rank, suit_t suit, bool face_up )
+void test_constructor( Card card, rank_t rank, suit_t suit )
 {
-    if( card.get_suit() != suit )
+    if( card.suit() != suit )
         cout << "Error: unexpected suit" << endl
              << "\tExpected: " << suit << endl
-             << "\tActual  : " << card.get_suit() << endl;
-    if( card.get_rank() != rank )
+             << "\tActual  : " << card.suit() << endl;
+    if( card.rank() != rank )
         cout << "Error: unexpected rank" << endl
              << "\tExpected: " << rank << endl
-             << "\tActual  : " << card.get_rank() << endl;
-    if( card.is_face_up() != face_up )
-        cout << "Error: unexpected face_up" << endl
-             << "\tExpected: " << face_up << endl
-             << "\tActual  : " << card.is_face_up() << endl;
+             << "\tActual  : " << card.rank() << endl;
 }
 

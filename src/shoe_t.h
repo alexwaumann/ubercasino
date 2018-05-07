271d58a722f72cd52a4b1f216bebc9f3a83bc76d
Alejandro Waumann <awaumann@gmail.com>
2018-05-07 10:15:33 -0500

    shoe_t enum class

---
+++ src/shoe_t.h
@@ -0,0 +1,17 @@
+/*
+ * File: shoe_t.h
+ * Created: 04/03/2018
+ * Modified: 04/03/2018
+ */
+
+#ifndef SHOE_T_H
+#define SHOE_T_H
+
+enum class shoe_t
+{
+    test,
+    eight,
+    infinite
+};
+
+#endif
